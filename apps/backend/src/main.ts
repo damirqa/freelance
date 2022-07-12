cdcae3b3a3c3b1370e9a637b25678e24f21865ca
damirqa <damirshaniyazov@gmail.com>
2022-07-12 21:15:39 +0500

    minor update

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,11 @@
-function start() {}
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+async function start() {
+    const port = process.env.PORT || 5001;
+    const app = await NestFactory.create(AppModule)
+
+    await app.listen(port, () => console.log(`Server started on port = ${port}`))
+}
 
 start();
